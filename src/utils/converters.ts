272b8b1c6a39112bfb9262c3825c7973de433598
Marcos Chen <marcostcchen@gmail.com>
2021-07-31 12:15:32 -0300

    Admin - historico done

--- src/utils/converters.ts
+++ src/utils/converters.ts
@@ -7,11 +7,45 @@ export const formatDateHour = (dateString: string) => {
   var year = date.getFullYear();  // yields year
   var hour = date.getHours();     // yields hours 
   var minute = date.getMinutes(); // yields minutes
+  var seconds = date.getSeconds();
 
   let minuteString = minute.toString();
   if (minute < 10) minuteString = "0" + minuteString
 
+  let secondString = seconds.toString();
+  if (seconds < 10) secondString = "0" + secondString
+
   if(day.toString() === 'NaN') return ' - '
 
-  return day + "/" + month + "/" + year + " " + hour + ':' + minuteString;
+  return day + "/" + month + "/" + year + " " + hour + ':' + minuteString + ":" + secondString;
+}
+
+export const formatDate = (dateString: string) => {
+  let date = new Date(dateString);
+
+  var day = date.getDate();       // yields date
+  var month = date.getMonth() + 1;    // yields month (add one as '.getMonth()' is zero indexed)
+  var year = date.getFullYear();  // yields year
+
+
+  if(day.toString() === 'NaN') return ' - '
+
+  return day + "/" + month + "/" + year;
+}
+
+export const formatHour = (dateString: string) => {
+  let date = new Date(dateString);
+  var hour = date.getHours();     // yields hours 
+  var minute = date.getMinutes(); // yields minutes
+  var seconds = date.getSeconds();
+
+  let minuteString = minute.toString();
+  if (minute < 10) minuteString = "0" + minuteString
+
+  let secondString = seconds.toString();
+  if (seconds < 10) secondString = "0" + secondString
+
+  if(hour.toString() === 'NaN') return ' - '
+
+  return hour + ':' + minuteString + ":" + secondString;
 }
--- src/views/Historico/HistoricoScreen.tsx
+++ src/views/Historico/HistoricoScreen.tsx
@@ -1,9 +1,10 @@
 import { DataGrid, GridColDef } from '@material-ui/data-grid'
+import { CircularProgress } from '@material-ui/core'
 import React, { useEffect, useState } from 'react'
 import Collapsible from 'react-collapsible'
 import "./HistoricoScreen.css"
 import * as fetchUtils from './fetch';
-import { formatDateHour, Status } from '../../utils';
+import { formatDate, formatDateHour, formatHour, Status } from '../../utils';
 import { toast } from 'react-toastify';
 import { UserWorkSession } from '../../models';
 
@@ -12,10 +13,13 @@ interface Props {
 }
 
 const columns: GridColDef[] = [
-  { field: 'regionName', headerName: 'Região', width: 150 },
-  { field: 'startWorkingTime', headerName: 'Início', width: 200, },
-  { field: 'startRestingTime', headerName: 'Início Descanço', width: 200, },
-  { field: 'finishRestingTime', headerName: 'Fim Descanço', width: 200, },
+  { field: 'regionName', headerName: 'Região', width: 160 },
+  { field: 'date', headerName: 'Data', width: 120 },
+  { field: 'startWorkingTime', headerName: 'Início', width: 120, },
+  { field: 'startRestingTime', headerName: 'I.Descanso', width: 160, },
+  { field: 'finishRestingTime', headerName: 'F.Descanso', width: 160, },
+  { field: 'workedTime', headerName: 'T.Trabalhado(s)', width: 180, },
+  { field: 'restedTime', headerName: 'T.Descansado(s)', width: 190, },
   { field: 'maxStayMinutes', headerName: 'T.Máx.Permitido(s)', width: 200 },
   { field: 'minRestMinutes', headerName: 'T.Mín.Descanso(s)', width: 200, },
 ];
@@ -40,42 +44,55 @@ export const HistoricoScreen: React.FC<Props> = () => {
       toast.error(res.message, { position: "bottom-right" });
       return;
     }
-    setListUserWorkSession(res.usersWorkingSessions); 
+
+    setIsLoading(false)
+    setListUserWorkSession(res.usersWorkingSessions);
   }
 
   return (
     <div className="DivHistorico">
       <h2>Histórico de usuários</h2>
-      <div style={styles.container}>
-        {listUserWorkSession.map((userWorkSession, index) => {
-          let rows: any = [];
-          userWorkSession.listWorkSessions.map(workSession => {
-            let row = {
-              id: index + 1 + Math.random(),
-              regionName: workSession.regionName,
-              maxStayMinutes: workSession.maxStayMinutes,
-              minRestMinutes: workSession.minRestMinutes,
-              startWorkingTime: formatDateHour(workSession.startWorkingTime),
-              startRestingTime: formatDateHour(workSession.startRestingTime),
-              finishRestingTime: formatDateHour(workSession.finishRestingTime)
-            }
-            rows.push(row);
-          })
+      {isLoading && (
+        <div style={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <CircularProgress size={30} />
+        </div>
+      )}
 
-          return (
-            <Collapsible key={index} trigger={
-              <div style={styles.title}>
-                <div style={{ width: "79%" }}>{userWorkSession.name}</div>
-                <div style={{ width: "20%", textAlign: 'right' }}>v</div>
-              </div>
-            }>
-              <div style={{ height: 400, width: '100%' }}>
-                <DataGrid rows={rows} columns={columns} pageSize={5} />
-              </div>
-            </Collapsible>
-          )
-        })}
-      </div>
+      {!isLoading && (
+        <div style={styles.container}>
+          {listUserWorkSession.map((userWorkSession, index) => {
+            let rows: any = [];
+            userWorkSession.listWorkSessions.map(workSession => {
+              let row = {
+                id: index + 1 + Math.random(),
+                regionName: workSession.regionName,
+                maxStayMinutes: workSession.maxStayMinutes,
+                minRestMinutes: workSession.minRestMinutes,
+                workedTime: ((new Date(workSession.startRestingTime).getTime() - new Date(workSession.startWorkingTime).getTime())/1000).toFixed(0),
+                restedTime: ((new Date(workSession.finishRestingTime).getTime() - new Date(workSession.startRestingTime).getTime())/1000).toFixed(0),
+                date: formatDate(workSession.startWorkingTime), 
+                startWorkingTime: formatHour(workSession.startWorkingTime),
+                startRestingTime: formatHour(workSession.startRestingTime),
+                finishRestingTime: formatHour(workSession.finishRestingTime)
+              }
+              rows.push(row);
+            })
+
+            return (
+              <Collapsible key={index} trigger={
+                <div style={styles.title}>
+                  <div style={{ width: "79%" }}>{userWorkSession.name}</div>
+                  <div style={{ width: "20%", textAlign: 'right' }}>v</div>
+                </div>
+              }>
+                <div style={{ height: 400, width: '100%' }}>
+                  <DataGrid rows={rows} columns={columns} pageSize={5} />
+                </div>
+              </Collapsible>
+            )
+          })}
+        </div>
+      )}
     </div>
   )
 }
